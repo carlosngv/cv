2e763ef2e7e7d038231c4fb106424fa65f159dcc
carlosngv <carlosngv99@hotmail.com>
2021-03-09 22:38:18 -0600

    Mail service implemented.

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const {sendMail} = require('./controllers/mail.controller');
 
 app.set('view engine', 'hbs');
+app.use(express.json());
 app.use(express.static('public'));
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -20,6 +22,17 @@ app.get('*', (req, res) => {
     res.render('404');
 });
 
+app.post('/sendMail', (req, res) => {
+    console.log(req.body);
+    const { email, message, name} = req.body
+     sendMail(email, message, name, (res) => {
+        console.log(res);
+    });
+
+    res.status(202).json({
+        msg: "ok"
+    })
+})
 
 
 app.listen(process.env.PORT, () => {
---
+++ controllers/mail.controller.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+
+ async function sendMail(email,message,name, callback) {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "carlosngv99@gmail.com",
+      pass: "amarilda7junio",
+    },
+  });
+
+  try {
+    var mailOptions = {
+        from: 'carlosngv99@gmail.com',
+        to: 'carlosngv99@gmail.com',
+        subject: "Carlos NG - Website | " + name,
+        text: message + ' | Correo: ' + email,
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      let info = await transporter.sendMail(mailOptions);
+      callback(info);
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+module.exports = {sendMail};
